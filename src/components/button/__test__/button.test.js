ea2cfbba738214e8dbe17782a8101badeca1b1ff
lelinh710 <lemylinh710@gmail.com>
2021-12-11 21:36:26 +0700

    add snapshot test

--- src/components/button/__test__/button.test.js
+++ src/components/button/__test__/button.test.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '../button';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
+import renderer from 'react-test-render';
+
+afterEach(cleanup);
 
 it('render without crashing', () => {
     const div = document.createElement("div");
@@ -14,4 +17,13 @@ it('render without crashing', () => {
 //     expect(getByTestID('button')).toHaveTextContent('Click me!');
 // })
 
+// it('render button correctly', () => {
+//     const {getByTestID} =  render(<Button label='save'/>);
+//     expect(getByTestID('button')).toHaveTextContent('save');
+// })
+
+it('match snapshot', () => {
+    const tree = renderer.createRenderer(<Button label='save'/>).toJSON();
+    expect(tree).toMatchSnapshot();
+})
 
